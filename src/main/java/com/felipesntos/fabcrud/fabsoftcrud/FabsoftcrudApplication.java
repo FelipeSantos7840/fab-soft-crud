533774964e59b2561ba8d898fd417294924f793a
Felipe dos Santos <felipe.santosmf2022@gmail.com>
2025-04-08 21:28:40 -0300

    Preparando ambiente para o CRUD

---
+++ src/main/java/com/felipesntos/fabcrud/fabsoftcrud/FabsoftcrudApplication.java
@@ -0,0 +1,13 @@
+package com.felipesntos.fabcrud.fabsoftcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabsoftcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabsoftcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipesntos/fabcrud/fabsoftcrud/FabsoftcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesntos.fabcrud.fabsoftcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabsoftcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
